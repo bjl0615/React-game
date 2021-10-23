3f883e0b645ec1e7f8fabcc62025fa118317e46f
bjl0615 <77454091+bjl0615@users.noreply.github.com>
2021-10-23 22:26:17 +0900

    Update getNumbers, onsubmitForm

--- NumberBaseball/NumberBaseball.jsx
+++ NumberBaseball/NumberBaseball.jsx
@@ -1,42 +1,78 @@
 import React, { Component } from "react";
 import Try from "./Try";
 
-function  getNumbers() {
-
-}    
+const getNumbers = () => {
+    const candidates = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+    const array = [];
+    for (let i = 0; i < 4; i += 1) {
+        const chosen = candidates.splice(Math.floor(Math.random() * (9 - i)), 1)[0];
+        array.push(chosen);
+    }
+    return array;
+}; 
 
 class NumberBaseball extends Component {
         state = {
             result : '',
             value : '' ,
-            answer : getNumbers(),
-            tries : [],
+            answer : getNumbers(), // ex) {1,3,5,7}
+            tries : [], //push를 쓰면 안됨
         };
 
    
 
     // Arrow function을 안 쓰면 this를 쓰지 못한다.
     onSubmitForm = (e) => {
+        const { value , tries , answer} = this.state;
         e.preventDefault();
-        console.log(this.state.value);
+        if(this.state.value === this.state.answer.join('')){
+            this.setState( {
+                result : '홈런',
+                tries : [...this.state.tries, {try: this.state.value, result : '홈런!'}],
+            });
+            alert('게임을 다시 시작합니다.');
+                this.setState( {
+                    value : '',
+                    answer: getNumbers(),
+                    tries : []
+            })
+        }else { // 답 틀렸으면
+            const answerArray = this.state.value.split('').map( (v) => parseInt(v));
+            let strike = 0;
+            let ball = 0;
+            if(this.state.tries.length >= 9) { //10번이상 틀렸을 때
+                this.setState({
+                    result : `10번 넘게 풀어서 실패! 답은 ${answer.join(',')}였습니다!`
+                });
+                alert('게임을 다시 시작합니다.');
+                this.setState( {
+                    value : '',
+                    answer: getNumbers(),
+                    tries : []
+                })
+            } else {
+                for(let i =0; i < 4; i += 1) {
+                    if(answerArray[i] === this.state.answer[i]) {
+                        strike += 1;
+                    } else if (answer.includes(answerArray[i])) {
+                        console.log('ball', answerArray[i], answer.indexOf(answerArray[i]));
+                        ball += 1;
+                    }
+                }
+                this.setState({
+                    tries: [this.state.tries, {try : this.state.value, result : `${strike} 스트라이크, ${ball} 볼입니다.`}],
+                    value : '',
+                })
+            }
+        }
     };
 
     onChangeInput = (e) => {
+        console.log(this.state.answer);
         this.setState( {
             value : e.target.value,
         });
     };
-
-    fruits = [
-        { fruit : '사과', taste : '맛있다'},
-        { fruit : '감', taste : ''},
-        { fruit : '귤', taste : ''},
-        { fruit : '밤', taste : ''},
-        { fruit : '배', taste : ''},
-        { fruit : '무', taste : ''},
-        { fruit : '사과', taste : ''},
-    ]
-
 
     render() {
         return (
@@ -48,14 +84,14 @@ class NumberBaseball extends Component {
                 <div>시도 : {this.state.tries.length}</div>
                 <ul>
                     {/* React에서는 반복문이 map이다. */}
-                    {this.fruits.map( (v , i) => {
+                    {this.state.tries.map( (v , i) => {
                             // 리엑트가 key를 보고 같은 컴포넌트인지 아닌지 판단한다.
                             // key 값은 고유한 값만 가져야 한다.
                             // Arrow function 에서는 return을 생략해도 된다.
                         return (
                             // 여기서 문제가 생기는데 Try에 값을 전달하지 못한다 이럴때 쓰이는 것이 props이다.
                             // props로 값을 전달해주는 대가로 Try는 NumberBaseball의 자식이 된다.
-                            <Try key={v.fruit + v.taste} value={v} index={i}/>
+                            <Try key={`${i + 1}차 시도 : `} tryInfo={v}/>
                         )
                     })}
                     <li></li>
