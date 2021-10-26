4575c035d5c848f0d7b8ccac402671127f3b2b93
bjl0615 <77454091+bjl0615@users.noreply.github.com>
2021-10-26 10:36:11 +0900

    Update memo

--- NumberBaseball/HooksTry.jsx
+++ NumberBaseball/HooksTry.jsx
@@ -1,12 +1,27 @@
-import React from "react";
+import React, { PureComponent, memo } from "react";
 
-const Try = ({ tryInfo }) => {
+// class Try extends PureComponent {
+//   render(){
+//     const { tryInfo } = this.props;
+//     return (
+//       <li>
+//         <div>{tryInfo.try}</div>
+//         <div>{tryInfo.result}</div>
+//       </li>
+//     )
+//   }
+// };
+
+// class로 할때는 PureComponent , memo 
+// Hooks로 할때는 memo
+
+const Try = memo(({ tryInfo }) => {
     return (
         <li>
         <div>{tryInfo.try}</div>
         <div>{tryInfo.result}</div>
       </li>
     )
-};
+});
 
 export default Try;
