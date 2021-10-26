51226356420f8c7ec1ccf912e00d82cca09e0370
bjl0615 <77454091+bjl0615@users.noreply.github.com>
2021-10-26 10:35:47 +0900

    Update PureComponent

--- NumberBaseball/RenderTest.jsx
+++ NumberBaseball/RenderTest.jsx
@@ -1,21 +1,29 @@
-import React, {Component} from "react";
+import React, { PureComponent } from "react";
 
-class Test extends Component {
+// PureComponent란 shouldComponentUpdate를 알아서 구현한 것
+class Test extends PureComponent {
     state = {
         counter : 0,
+        string : 'hello',
+        number : 1,
+        boolean : true,
+        object : {},
+        array : [],
     };
 
     // React에서 state값이 바뀌어야 렌더링하게 도와주는 함수
-    shouldComponentUpdate(nextProps, nextState, nextContext) {
-        if (this.state.counter !== nextState.counter) {
-            return true;
-        }
-        return false;
-    };
+    // shouldComponentUpdate(nextProps, nextState, nextContext) {
+    //     if (this.state.counter !== nextState.counter) {
+    //         return true;
+    //     }
+    //     return false;
+    // };
+
+    // { a : 1 }에서 setState { a : 1 }을 할 때 새로 렌더링하므로 state에 객체 구조를 안 쓰는 게 좋다.
 
     onClick = () => {
         this.setState({
-
+            array: [...this.state.array , 1]
         });
     };
 
