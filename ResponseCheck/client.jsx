569e60c2448e3dce60fda027fd74100d02fd64dc
bjl0615 <77454091+bjl0615@users.noreply.github.com>
2021-10-27 20:33:23 +0900

    Update Hooks

--- ResponseCheck/client.jsx
+++ ResponseCheck/client.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import ResponseCheck from './ResponseCheck';
+import ResponseCheck from './Hooks';
 
 ReactDOM.render(<ResponseCheck />, document.querySelector('#root'));
