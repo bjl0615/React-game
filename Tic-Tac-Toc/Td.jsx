1086a9bf9176a8606052cfc5da8741517566bdeb
bjl0615 <77454091+bjl0615@users.noreply.github.com>
2021-11-04 22:23:23 +0900

    Add memo

--- Tic-Tac-Toc/Td.jsx
+++ Tic-Tac-Toc/Td.jsx
@@ -1,7 +1,15 @@
-import React, {useCallback} from "react";
+import React, {memo, useCallback , useEffect , useRef} from "react";
 import { CLICK_CELL } from "./TicTacToe";
 
-const Td = ({ rowIndex , cellIndex , dispatch , cellData }) => {
+const Td = memo(({ rowIndex , cellIndex , dispatch , cellData }) => {
+    console.log('td rendered')
+
+    const ref = useRef([]);
+    useEffect(() => {
+        console.log(rowIndex === ref.current[0], cellIndex === ref.current[1], dispatch === ref.current[2], cellData === ref.current[3])
+        ref.current = [rowIndex, cellIndex, dispatch, cellData];
+    }, [rowIndex , cellIndex, dispatch, cellData])
+
     const onClickTd = useCallback(() => {
         console.log(rowIndex,cellIndex);
         if (cellData) {
@@ -10,9 +18,11 @@ const Td = ({ rowIndex , cellIndex , dispatch , cellData }) => {
         dispatch({ type: CLICK_CELL, row: rowIndex , cell: cellIndex });
     }, [cellData])
 
+
+
     return (
         <td onClick={onClickTd}>{cellData}</td>
     )
-};
+});
 
 export default Td;
