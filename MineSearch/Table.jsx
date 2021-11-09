068ac021e5bbcab92d3b806e75bc4dcb0b88db15
bjl0615 <77454091+bjl0615@users.noreply.github.com>
2021-11-09 15:36:50 +0900

    Update MineSearch

--- MineSearch/Table.jsx
+++ MineSearch/Table.jsx
@@ -1,14 +1,14 @@
-import React, { useContext } from "react";
-import Tr from "./Tr";
-import { TableContext } from "./MineSearch";
+import React, { useContext, memo } from 'react';
+import Tr from './Tr';
+import { TableContext } from './MineSearch';
 
-const Table = () => {
-    const { tableData } = useContext(TableContext);
-    return (
-        <table>
-            {Array(tableData.length).fill().map((tr, i) => <Tr rowIndex = {i}/>)}
-        </table>
-    )
-}
+const Table = memo(() => {
+  const { tableData } = useContext(TableContext);
+  return (
+    <table>
+      {Array(tableData.length).fill().map((tr, i) => <Tr rowIndex={i} />)}
+    </table>
+  )
+});
 
 export default Table;
